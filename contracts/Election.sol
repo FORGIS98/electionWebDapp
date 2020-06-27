d537f86a49614d416432d0efa3514aa165bdd0a2
FORGIS98 <cifradoforgis@gmail.com>
2020-06-27 21:10:07 +0200

    More candidates and addCandidate function

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -3,10 +3,32 @@ pragma solidity ^0.6.10; // Solidity version (latest - Jun 2020)
 
 contract Election { // Contract declaration
 
-  string public candidate; // Solidity gives you a getter automatically?
+  // Model Candidate
+  struct Candidate{
+    uint id;
+    string name;
+    uint voteCount;
+  }
+
+  // Mapping (is like a Hash Map in Java or diccionary in Python)
+  // Here we store all candidates struc
+  mapping(uint => Candidate) public candidates; // id => candidate
+
+  // Amount of candidates (it will help with the mapping)
+  // (We can't iterate on mappìngs)
+  uint public candidatesCount;
 
   // Constructor (runs one time when contract is deploy)
   constructor() public {
-    candidate = "Candidate 1";
+    addCandidate("Satoshi Nakamoto");
+    addCandidate("Dennis Ritchie");
+    addCandidate("Linus Torvalds");
+  }
+
+  // Adds a candidate to the maping
+  // underscore for local variables
+  function addCandidate(string memory _name) private {
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    candidatesCount += 1;
   }
 }
